0b1924ce8dff8b8c1a22f45c32c00b497cce84a0
User <braxtoneidem@gmail.com>
2020-11-24 20:13:10 -0800

    big revision on processIndustry

--- EidemAssignment3.java
+++ EidemAssignment3.java
@@ -22,8 +22,6 @@ class EidemAssignment3 {
     static Connection writerConn = null;
     // To Do: Other variables
 
-        
-
     static final String getDatesQuery = "select max(startDate), min(endDate)"
             + "  from (select Ticker, min(TransDate) as StartDate, max(TransDate) as endDate,"
             + "            count(distinct TransDate) as tradingDays" + "          from company natural join pricevolume"
@@ -64,6 +62,18 @@ class EidemAssignment3 {
             this.endDate = endDay;
             this.commonDays = commonDays;
         }
+
+        public List<String> getTicker() {
+            return tickerList;
+        }
+
+        public String getStartDate() {
+            return startDate;
+        }
+
+        public String getEndDate() {
+            return endDate;
+        }
     }
 
     public static void main(String[] args) throws Exception {
@@ -90,13 +100,11 @@ class EidemAssignment3 {
                 System.out.printf("Processing %s%n", industry);
                 IndustryData iData = processIndustry(industry);
                 if (iData != null && iData.tickerList.size() > 2) {
-                System.out.printf("%d accepted tickers for %s(%s - %s), %d common dates%n",
-                iData.tickerList.size(), industry, iData.startDate, iData.endDate,
-                iData.commonDays);
-                processIndustryGains(industry, iData);
-                }
-                else{
-                System.out.printf("Insufficient data for %s => no analysis%n", industry);
+                    System.out.printf("%d accepted tickers for %s(%s - %s), %d common dates%n", iData.tickerList.size(),
+                            industry, iData.startDate, iData.endDate, iData.commonDays);
+                    // processIndustryGains(industry, iData);
+                } else {
+                    System.out.printf("Insufficient data for %s => no analysis%n", industry);
                 }
                 System.out.println();
             }
@@ -153,62 +161,159 @@ class EidemAssignment3 {
         return result;
     }
 
+    // static IndustryData processIndustry(String industry) throws SQLException {
+    // // To Do: Execute the appropriate SQL queries (you need two of them) and
+    // return
+    // // an object of IndustryData
+
+    // int numDays = -1;
+    // String startDate = "-1";
+    // String endDate = "-1";
+    // List<String> tickers = new ArrayList<>();
+
+    // Statement stmt = readerConn.createStatement();
+    // // ResultSet tickersResult = stmt.executeQuery("select Ticker from company
+    // where Industry = '" + industry + "'");
+
+    // // while (tickersResult.next()) {
+    // // String ticker = tickersResult.getString("Ticker");
+    // // //System.out.println(ticker);
+    // // tickers.add(ticker);
+    // // }
+
+    // // TODO:
+
+    // // Need to get the max start date and the min end date!
+    // // Should I go though that with using SQL or would it be easiser to go about
+    // it
+    // // with java code
+
+    // // all the common days will not have null for any company
+
+    // // ResultSet startDateResult = stmt.executeQuery("select Transdate from
+    // company
+    // // where Industry = '" + industry + "'");
+
+    // ResultSet startDateResult = stmt.executeQuery(
+    // "select max(startDate), min(endDate)"
+    // + " from (select Ticker, min(TransDate) as StartDate, max(TransDate) as
+    // endDate,"
+    // + " count(distinct TransDate) as tradingDays"
+    // + " from company natural join pricevolume" + " where Industry = '" + industry
+    // + "' "
+    // + " group by Ticker" + " having tradingDays >= StartDate) as TickerDates");
+
+    // // System.out.println();
+
+    // while (startDateResult.next()) {
+    // startDate = startDateResult.getString("max(startDate)");
+    // endDate = startDateResult.getString("min(endDate)");
+    // }
+
+    // // this query will get the min and max data of the common days and return the
+    // count
+    // ResultSet getTickerDatesQuery = stmt.executeQuery("select Ticker,
+    // min(TransDate) as StartDate, max(TransDate) as endDate,"
+    // + " count(distinct TransDate) as tradingDays" + " from company natural join
+    // pricevolume"
+    // + " where Industry = '" + industry + "' " + " and TransDate >= '" + startDate
+    // + "' and TransDate <= '" + endDate + "' " + " group by Ticker" + " having
+    // tradingDays >= '"
+    // + startDate + "' " + " order by Ticker");
+
+    // // getTickerDatesQuery = "select Ticker, min(TransDate) as StartDate,
+    // max(TransDate) as endDate,"
+    // // + " count(distinct TransDate) as tradingDays" + " from company natural
+    // join pricevolume"
+    // // + " where Industry = ?" + " and TransDate >= ? and TransDate <= ?" + "
+    // group by Ticker"
+    // // + " having tradingDays >= ?" + " order by Ticker";
+
+    // while (getTickerDatesQuery.next()) {
+    // numDays = Integer.parseInt(getTickerDatesQuery.getString("tradingDays"));
+
+    // // This was an attempt to fix how I am getting too many tickers
+    // // The result was it was giving me just a less
+    // // - could this be accounting for the null that the write up talks about
+
+    // String ticker = getTickerDatesQuery.getString("Ticker");
+    // System.out.println(ticker);
+    // tickers.add(ticker);
+    // }
+
+    // return new IndustryData(tickers, startDate, endDate, numDays);
+    // }
+
     static IndustryData processIndustry(String industry) throws SQLException {
         // To Do: Execute the appropriate SQL queries (you need two of them) and return
         // an object of IndustryData
 
-        int numDays = -1;
-        String startDate = "-1";
-        String endDate = "-1";
-        List<String> tickers = new ArrayList<>();
+
+
+        int minDataDays = 150; //per write-up
+        String minDataDay = "150";
+
 
         Statement stmt = readerConn.createStatement();
-        ResultSet tickersResult = stmt.executeQuery("select Ticker from company where Industry = '" + industry + "'");
 
-        while (tickersResult.next()) {
-            String ticker = tickersResult.getString("Ticker");
-            System.out.println(ticker);
-            tickers.add(ticker);
-        }
 
-        // TODO:
-
-        // Need to get the max start date and the min end date!
-        // Should I go though that with using SQL or would it be easiser to go about it
-        // with java code
-
-        // all the common days will not have null for any company
-
-        // ResultSet startDateResult = stmt.executeQuery("select Transdate from company
-        // where Industry = '" + industry + "'");
-
-        ResultSet startDateResult = stmt.executeQuery("select max(startDate), min(endDate)"
+        //this gets the min and max date that I need
+        String getDatesQuery = "select max(startDate), min(endDate)"
                 + "  from (select Ticker, min(TransDate) as StartDate, max(TransDate) as endDate,"
                 + "            count(distinct TransDate) as tradingDays"
-                + "          from company natural join pricevolume" + "          where Industry = '" + industry + "' "
-                + "          group by Ticker" + "          having tradingDays >= StartDate) as TickerDates");
+                + "          from company natural join pricevolume" + "          where Industry = ?"
+                + "          group by Ticker" + "          having tradingDays >= ?) as TickerDates";
 
-        // System.out.println();
+        PreparedStatement getDates = readerConn.prepareStatement(getDatesQuery);        
 
-        while (startDateResult.next()) {
-            startDate = startDateResult.getString("max(startDate)");
-            endDate = startDateResult.getString("min(endDate)");
+
+
+
+
+        //this uses the dates that were found 
+        String getTickerDatesQuery = "select Ticker, min(TransDate) as StartDate, max(TransDate) as endDate,"
+                + "      count(distinct TransDate) as tradingDays" + "  from company natural join pricevolume"
+                + "  where Industry = ?" + "    and TransDate >= ? and TransDate <= ?" + "  group by Ticker"
+                + "  having tradingDays >= ?" + "  order by Ticker";
+
+        PreparedStatement getTickerDates = readerConn.prepareStatement(getTickerDatesQuery);
+        
+
+        getDates.setString(1, industry);
+        getDates.setInt(2, minDataDays);
+        ResultSet rs = getDates.executeQuery();
+
+        String startDate;
+        String endDate;
+
+        if (rs.next()) {
+            startDate = rs.getString(1);
+            endDate = rs.getString(2);
+        } else {
+            return null;
         }
 
-        ResultSet getTickerDatesQuery = stmt
-                .executeQuery("select Ticker, min(TransDate) as StartDate, max(TransDate) as endDate,"
-                        + "      count(distinct TransDate) as tradingDays" + "  from company natural join pricevolume"
-                        + "  where Industry = '" + industry + "' " + "    and TransDate >= '" + startDate
-                        + "' and TransDate <= '" + endDate + "' " + 
-                        "  group by Ticker" + "  having tradingDays >= '" + startDate + "' " + "  order by Ticker");
+        rs.close();
 
+        getTickerDates.setString(1, industry);
+        getTickerDates.setString(4, minDataDay);
+        getTickerDates.setString(2, startDate);
+        getTickerDates.setString(3, endDate);
+        rs = getTickerDates.executeQuery();
 
-        while (getTickerDatesQuery.next()) {
-            numDays = Integer.parseInt(getTickerDatesQuery.getString("tradingDays"));
+        List<String> tickers = new ArrayList<>();
+        int numDays = Integer.MAX_VALUE;
+        while (rs.next()) {
+            String ticker = rs.getString(1);
+            int tickerDays = rs.getInt(4);
+            if (tickerDays < numDays) {
+                numDays = tickerDays;
+            }
         }
+        rs.close();
 
         return new IndustryData(tickers, startDate, endDate, numDays);
-    }
+    } // end processIndustry
 
     static void processIndustryGains(String industry, IndustryData data) throws SQLException {
         // To Do:
@@ -219,10 +324,37 @@ class EidemAssignment3 {
         // created earlier. You may use the following way to do that for each company
         // (or ticker) of an indsutry:
 
+        Statement stmt = readerConn.createStatement();
 
+        String indus = industry;
+        List<String> ticker = data.getTicker();
+        String startDate = data.getStartDate();
+        String endDate = data.getEndDate();
 
+        for (int i = 0; i < ticker.size(); i++) {
+            String currentTicker = ticker.get(i);
 
-        
+            // for current ticker
+            ResultSet currentTickerQuery = stmt.executeQuery("select P.TransDate, P.openPrice, P.closePrice "
+                    + "from pricevolume P " + "where Ticker = '" + currentTicker + "' and TransDate >= '" + startDate
+                    + "' " + "and TransDate <= '" + endDate + "' ");
+
+            // for rest of tickers
+
+            ResultSet allTickerQuery = stmt.executeQuery(
+                    "select P.TransDate, P.openPrice, P.closePrice " + "from pricevolume P natural join company "
+                            + "where Industry = '" + industry + "' " + "and TransDate >= '" + startDate
+                            + "' and TransDate <= '" + endDate + "' " + "order by TransDate, Ticker");
+
+        }
+
+        /*
+         * Trading intervals A stock will be considered for comparison if it has at
+         * least 150 days of data
+         * 
+         * 
+         */
+
         // insertPerformanceData.setString(1, industry);
         // insertPerformanceData.setString(2, ticker);
         // insertPerformanceData.setString(3, startdate);
