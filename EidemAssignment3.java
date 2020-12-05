ba65d8e07dd3b0c79fc4a5a10492a8930191f695
CircuitBreaker21 <BraxtonEidem@gmail.com>
2020-12-04 20:17:58 -0800

    just need to add database upload

--- EidemAssignment3.java
+++ EidemAssignment3.java
@@ -60,10 +60,7 @@ class EidemAssignment3 {
         String startDate, endDate;
         int commonDays;
 
-
-        ArrayList<TickerList> stocksList; 
-
-  
+        ArrayList<TickerList> stocksList;
 
         IndustryData(List<String> tickerList, String startDay, String endDay, int commonDays) {
             this.tickerList = tickerList;
@@ -72,20 +69,18 @@ class EidemAssignment3 {
             this.commonDays = commonDays;
         }
 
-        
-        IndustryData(String Industry, ArrayList<TickerList> Stocks){
+        IndustryData(String Industry, ArrayList<TickerList> Stocks) {
             industry = Industry;
             stocksList = Stocks;
         }
 
-        public ArrayList<TickerList> getTickersList(){
+        public ArrayList<TickerList> getTickersList() {
             return stocksList;
         }
-        
-        public void acountSplits(){
+
+        public void acountSplits() {
             for (int i = 0; i < stocksList.size(); i++) {
                 ArrayList<StockData> stocks = stocksList.get(i).getTickerList();
-
 
                 double scaleFactor = 1;
                 double prevOpenPrice = -1;
@@ -95,19 +90,16 @@ class EidemAssignment3 {
                 int scaleCount2 = 0; // 3:1
                 int scaleCount3 = 0; // 3:2
 
-
                 ArrayList<StockData> result = new ArrayList<StockData>();
                 for (int j = 0; j < stocks.size(); j++) {
                     StockData stock = stocks.get(j);
-                    
-                    
-                    //process data for individual ticker
+
+                    // process data for individual ticker
                     String ticker = stock.getTicker();
                     String transDate = stock.getDate();
                     double openPrice = stock.getOpenPrice();
                     double closePrice = stock.closePrice;
-                    
-                    
+
                     if (tradingDays == 0) {
                         prevOpenPrice = openPrice;
                         System.out.println(stock.getTicker());
@@ -152,47 +144,46 @@ class EidemAssignment3 {
                     prevOpenPrice = openPrice;
                     tradingDays++;
 
-
                 }
 
-                //Replace the return stock array with the one stored in stock list
+                // Replace the return stock array with the one stored in stock list
                 stocksList.get(i).replaceList(result);
 
-                //System.out.println(splitCount + " splits in " + tradingDays + " trading days");
+                // System.out.println(splitCount + " splits in " + tradingDays + " trading
+                // days");
             }
 
         }
 
-        public void printIndustryData(){
-            for(int i = 0; i < stocksList.size(); i++){
+        public void printIndustryData() {
+            for (int i = 0; i < stocksList.size(); i++) {
                 ArrayList<StockData> stocks = stocksList.get(i).getTickerList();
-                for(int j = 0; j < stocks.size(); j++){
+                for (int j = 0; j < stocks.size(); j++) {
                     StockData stock = stocks.get(j);
-                    System.out.println(industry + "\t" + stock.getTicker() + " \t" + stock.getDate() + "\t" + stock.getOpenPrice()
-                            + " - " + stock.getClosePrice());
-                    
+                    System.out.println(industry + "\t" + stock.getTicker() + " \t" + stock.getDate() + "\t"
+                            + stock.getOpenPrice() + " - " + stock.getClosePrice());
+
                 }
             }
         }
     }
 
-
-    public static class TickerList{
+    public static class TickerList {
         ArrayList<StockData> tickerList;
 
-        TickerList(){
+        TickerList() {
             tickerList = new ArrayList<StockData>();
         }
 
-        public void addStockData(StockData stock){
+        public void addStockData(StockData stock) {
             tickerList.add(stock);
         }
 
-        public ArrayList<StockData> getTickerList(){
+        public ArrayList<StockData> getTickerList() {
             return tickerList;
         }
 
-        public void replaceList(ArrayList<StockData> result){
+        public void replaceList(ArrayList<StockData> result) {
             tickerList = result;
         }
     }
@@ -203,26 +194,26 @@ class EidemAssignment3 {
         double openPrice;
         double closePrice;
 
-        StockData(String Ticker, String Date, double OpenPrice, double ClosePrice){
+        StockData(String Ticker, String Date, double OpenPrice, double ClosePrice) {
             ticker = Ticker;
             date = Date;
             openPrice = OpenPrice;
             closePrice = ClosePrice;
         }
 
-        public String getTicker(){
+        public String getTicker() {
             return ticker;
         }
 
-        public String getDate(){
+        public String getDate() {
             return date;
         }
 
-        public Double getOpenPrice(){
+        public Double getOpenPrice() {
             return openPrice;
         }
 
-        public Double getClosePrice(){
+        public Double getClosePrice() {
             return closePrice;
         }
 
@@ -311,12 +302,6 @@ class EidemAssignment3 {
 
         return result;
     }
-
- 
-
-
-
-
 
     static IndustryData processIndustry(String industry) throws SQLException {
         // To Do: Execute the appropriate SQL queries (you need two of them) and return
@@ -412,141 +397,169 @@ class EidemAssignment3 {
         getIndustryPriceData.setString(3, data.endDate);
 
         ResultSet rs = getIndustryPriceData.executeQuery();
-        
-        
-        
+
         String tickerQuery = "-1";
         String transDateQuery = "-1";
         String openPriceQuery = "-1";
         String closePriceQuery = "-1";
-        
-        ArrayList<StockData> stocks = new ArrayList<StockData>(); 
 
-
-
-
-
-        Boolean firstDay = true;
-        String firstDate = "";
+       
 
         HashMap<String, Integer> tickerIndexHash = new HashMap<String, Integer>();
 
         ArrayList<String> tickers = new ArrayList<String>();
+
+
+
+
         ArrayList<TickerList> tickersList = new ArrayList<TickerList>();
 
-
-        while(rs.next()){
+        while (rs.next()) {
             tickerQuery = rs.getString("Ticker");
             transDateQuery = rs.getString("TransDate");
             openPriceQuery = rs.getString("OpenPrice");
             closePriceQuery = rs.getString("ClosePrice");
 
-            if(!tickerIndexHash.containsKey(tickerQuery)){
+            if (!tickerIndexHash.containsKey(tickerQuery)) {
 
             }
 
-            if(!tickers.contains(tickerQuery)){
+            if (!tickers.contains(tickerQuery)) {
                 tickers.add(tickerQuery);
 
                 tickersList.add(new TickerList());
-                //tickersList.add(tickers);
+                // tickersList.add(tickers);
             }
 
-            
-            //make a class of this data and make an arraylist to hold that class
-            //consider the flow of dates
-            //System.out.println(industry + "\t" + tickerQuery + " \t" + transDateQuery + "\t" + openPriceQuery + " - " + closePriceQuery);
-            
-            
+            // make a class of this data and make an arraylist to hold that class
+            // consider the flow of dates
+            // System.out.println(industry + "\t" + tickerQuery + " \t" + transDateQuery +
+            // "\t" + openPriceQuery + " - " + closePriceQuery);
+
             int tickerNumber = tickers.indexOf(tickerQuery);
-            StockData stock = new StockData(tickerQuery, transDateQuery, Double.parseDouble(openPriceQuery), Double.parseDouble(closePriceQuery));
-            
+            StockData stock = new StockData(tickerQuery, transDateQuery, Double.parseDouble(openPriceQuery),
+                    Double.parseDouble(closePriceQuery));
+
             tickersList.get(tickerNumber).addStockData(stock);
 
+            // TODO:
+            // Make an arrayList that holds 60 data points at a time
 
-
-            //TODO:
-            //Make an arrayList that holds 60 data points at a time 
-
-            //make a class of industry interval
-            //make a list of that class 
-            //have that class have a return that is what I want to put into my database
+            // make a class of industry interval
+            // make a list of that class
+            // have that class have a return that is what I want to put into my database
         }
 
         IndustryData industryList = new IndustryData(industry, tickersList);
 
-        
+        // TODO
+        // Must account for splits before continuing
 
-       
+        // maybe make an arrraylost
 
+        industryList.acountSplits();
 
-        //TODO
-        //Must account for splits before continuing 
+        // TODO:
+        // Ticker Return math
 
+        ArrayList<TickerList> stocksList = industryList.getTickersList();
 
-            //maybe make an arrraylost
-
-            industryList.acountSplits();
-
-
-
-
-        //TODO:
-        //Ticker Return math
+        System.out.println(stocksList.size());
 
 
+        if(industry.equals("Telecommunications Services")){
+           
 
 
-            ArrayList<TickerList> stocksList = industryList.getTickersList();
+        for (int i = 0; i < stocksList.size(); i++) {  //for each ticker
 
-            for (int i = 0; i < stocksList.size(); i++) {
-                ArrayList<StockData> stocksClass = stocksList.get(i).getTickerList();
-
-
-                //need to go though the current ticker and get the date bounds for each interval
-                //      also need to get the start and end prices for that interval 
-
-                int intervalStart = 0;
-                int intervalEnd = intervalStart + 60;
-
-                String StartDate;
-                String EndDate;
-
-                Double TickerReturn;
-                Double IndustryReturn;
+            ArrayList<StockData> stocksClass = stocksList.get(i).getTickerList(); //entry list of current ticker
 
 
+            int index=0;
 
-                //Dont need forloops to go though the whole ticker set, just need the start and end ticker
+            while(index < stocksClass.size()){ // while current ticker has trading days
 
-                for (int j = intervalStart; j < intervalEnd; j++) {
-                    StockData stock = stocksClass.get(j);
+                int difference;
+                if((index + 60) < stocksClass.size()){
+                    difference = 60;
+                } else {
+                    difference = stocksClass.size() - index - 1;
                     
-
                 }
 
-                //ned to go though the rest if the tickers and get the same data 
-                for (int j = 0; j < stocksList.size(); j++) {
-                    ArrayList<StockData> stocksClassExclusive = stocksList.get(j).getTickerList();
-                    while(j != i){ //Filter out current stock with rest of stocks 
+                System.out.println(index);
+                
+                //get start and end date and prices for current ticker in industry 
+                String currentTicker = stocksClass.get(index).getTicker();
+                System.out.println("Current ticker: " + currentTicker);
+                
+                
 
 
-                        // Dont need forloops to go though the whole ticker set, just need the start and
-                        // end ticker
-                        for (int k = intervalStart; k < intervalEnd; k++) {
-                            StockData stock = stocksClassExclusive.get(k);
+                double startPrice = stocksClass.get(index).getOpenPrice();
+                double endPrice = stocksClass.get(index + difference).getClosePrice();
+
+                double tickerRetrun = (endPrice / startPrice) - 1;
+
+
+                String startDate = stocksClass.get(index).getDate();
+                String endDate = stocksClass.get(index + difference).getDate(); // the start of the next trading interval
+                
+                
+
+                System.out.println(startDate + " - " + endDate);
+
+                
+
+                double industryReturn = -1;
+
+                double sumClosePrice = 0;
+                double sumOpenPrice = 0;
+
+                //for the rest of the tickers 
+                for(int j = 0; j < stocksList.size(); j++){
+                    if(i == j){ // if i = j, do nothing, they are the same ticker
+                        //samw ticker as the one 
+                    } else {
+                        ArrayList<StockData> curIndustryStock = stocksList.get(j).getTickerList();
+                        //add prices to summation equations once their dates are checked   
+                        System.out.println("other ticker: " + j + " " + curIndustryStock.get(j).getTicker() + " " + stocksList.size()); 
+                        
+
+                        //Adding prices of every other industry
+                        System.out.println("adding to price sum");
+                        sumClosePrice += curIndustryStock.get(j).getClosePrice();
+                        sumOpenPrice += curIndustryStock.get(j).getOpenPrice();
 
 
 
-    
-                        }
-
+                        System.out.println(sumClosePrice);
+                        
                     }
-
-                    // ned to go though the rest if the tickers and get the same data
-
                 }
-            }
+
+
+                industryReturn = (sumClosePrice / sumOpenPrice) - 1;
+                ///(1/ (stocksList.size() - 1)) * 
+                
+                //end of interval output 
+                
+                System.out.println(industry + " | " + currentTicker + " | " + startDate + " | " + endDate + " | " + tickerRetrun + " | " + industryReturn);
+
+
+
+                index += 60;
+             }
+                //process the summation equation s
+
+
+
+
+
+
+
+                //print all data to the database
 
 
 
@@ -555,16 +568,70 @@ class EidemAssignment3 {
 
 
 
-        //TODO:
-        //Industry return 
+
+            // need to go though the current ticker and get the date bounds for each
+            // interval
+            // also need to get the start and end prices for that interval
+
+            // Dont need forloops to go though the whole ticker set, just need the start and
+            // end ticker
+
+            // for (int j = intervalStart; j < intervalEnd; j++) {
+            // StockData stock = stocksClass.get(j);
+
+            // }
+
+            // having trouble with checking of the interval is withing the cororet about if
+            // the thime alloted
+
+            // int intervalEnd = 60;
+            // int intervalIndex = 0;
+            // int intervalStart = 0;
+            // while (intervalEnd <= stocksClass.size()) {
+
+            //     String startDate;
+            //     String endDate;
+
+            //     Double tickerReturn;
+            //     Double industryReturn;
+
+            //     Double tickerOpenPrice = stocksClass.get(intervalStart).getOpenPrice();
+            //     Double tickerClosingPrice = stocksClass.get(intervalEnd).getClosePrice();
+
+            //     tickerReturn = (tickerOpenPrice / tickerClosingPrice)-1;
+
+            //     //System.out.println("Current Ticker: " + stocksClass.get(i).getTicker() + "\t");
+
+            //     // ned to go though the rest if the tickers and get the same data
+            //     for (int j = 0; j < stocksList.size(); j++) {
+            //         ArrayList<StockData> stocksClassExclusive = stocksList.get(j).getTickerList();
 
 
+            //         System.out.println(stocksClassExclusive.size());
+            //         if (j != i) { // Filter out current stock with rest of stocks
+            //             System.out.println("Other Ticker: " + stocksClassExclusive.get(j).getTicker());
 
+            //             // Dont need forloops to go though the whole ticker set, just need the start and
+            //             // end ticker
+            //             // for (int k = intervalStart; k < intervalEnd; k++) {
+            //             // StockData stock = stocksClassExclusive.get(k);
 
+            //             // }
 
+            //         }
 
+            //         // ned to go though the rest if the tickers and get the same data
 
+            //     }
+            //     intervalIndex++;
+            //     intervalEnd = intervalStart + 60 + (60 * intervalIndex);
+            //     intervalStart = 0 + (60 * intervalIndex);
+            // }
+        }
+        }
 
+        // TODO:
+        // Industry return
 
         // insertPerformanceData.setString(1, industry);
         // insertPerformanceData.setString(2, ticker);
@@ -574,11 +641,5 @@ class EidemAssignment3 {
         // insertPerformanceData.setString(6, String.format("%10.7f", industryReturn);
         // int result = insertPerformanceData.executeUpdate();
     }
-
-
-
-
-
-
 
 }
